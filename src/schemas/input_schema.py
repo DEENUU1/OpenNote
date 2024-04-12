d9b93e0c77ca268f565cd385c9af1ad18a5d22be
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-04-12 16:16:53 +0200

    feature: create schemas for InputData model

--- src/schemas/input_schema.py
+++ src/schemas/input_schema.py
@@ -1,5 +1,32 @@
 from pydantic import BaseModel
+from models.input import TypeEnum, StatusEnum
+from typing import Optional, List
+from datetime import datetime
 
 
 class InputDataInput(BaseModel):
-    type:
+    type: TypeEnum
+    text: Optional[str] = None
+    article_url: Optional[str] = None
+    youtube_url: Optional[str] = None
+    file_path: Optional[str] = None
+    status: StatusEnum = StatusEnum.NEW
+
+
+class InputDataOutput(BaseModel):
+    id: int
+    type: TypeEnum
+    text: Optional[str] = None
+    article_url: Optional[str] = None
+    youtube_url: Optional[str] = None
+    file_path: Optional[str] = None
+    status: StatusEnum
+    created_at: datetime
+    updated_at: Optional[datetime] = None
+
+
+class InputDataListOutput(BaseModel):
+    prev_page: Optional[int]
+    next_page: Optional[int]
+    data: List[Optional[InputDataOutput]]
+
