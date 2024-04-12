172e7d8c1c5d811b20ab82c9d39d5ab22555a5de
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-04-12 16:13:27 +0200

    feature: add enum types to InputData model

--- src/models/input.py
+++ src/models/input.py
@@ -1,18 +1,37 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, func
 from sqlalchemy.orm import relationship
 from config.database import Base
+from sqlalchemy import Enum as SQLAlchemyEnum
+from enum import Enum
 
 
-class Input(Base):
+class TypeEnum(str, Enum):
+    TEXT = "text"
+    YOUTUBE = "youtube"
+    FILE = "file"
+    ARTICLE = "article"
+
+
+class StatusEnum(str, Enum):
+    NEW = "new"
+    PREPROCESSING = "preprocessing"
+    PROCESSING = "processing"
+    FAILED = "failed"
+    DONE = "done"
+
+
+class InputData(Base):
     __tablename__ = 'input'
 
     id = Column(Integer, primary_key=True)
-    type = Column(String)
+    type = Column(SQLAlchemyEnum(TypeEnum), nullable=False)
     input_string = Column(String)
     article_url = Column(String)
     youtube_url = Column(String)
     file_path = Column(String)
-    status = Column(String)
+    status = Column(SQLAlchemyEnum(StatusEnum), nullable=False)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
     preprocess_data = relationship("PreprocessData", uselist=False, back_populates="input")
     processed_data = relationship("ProcessedData", uselist=False, back_populates="input")
